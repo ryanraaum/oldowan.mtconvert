611700b189aab428eeffec98618e7c789bbd6bd6
Ryan Raaum <github@raaum.org>
2009-05-14 14:31:05 -0400

    files for importer functions

---
+++ oldowan/mtconvert/importer.py
@@ -0,0 +1,19 @@
+import csv
+
+def load_csv(file,
+             header         = True,  # column number if present
+             hvr1           = False, # column number if present
+             add16k         = True,  # add 16000 to every hvr1 site?
+             hvr2           = False, # column number if present
+             rflps          = False, # column number if present
+             rflp_format    = False, # ? how to implement this ?
+             sites          = False, # column number(s) if present
+             sites_on_rCRS  = False, # matched entry or list for sites columns
+             haplogroup     = False, # column number if present
+             sample_id      = False, # column number if present
+             haplotype_id   = False, # column number if present
+             n              = False, # column number if present
+             ):
+    """Load mitochondrial haplotype definitions from csv file."""
+
+    pass
---
+++ tests/test_importer.py
@@ -0,0 +1,5 @@
+from oldowan.mtconvert.importer import load_csv
+
+def test_true():
+    assert True
+
