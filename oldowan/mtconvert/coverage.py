ce05c6b5a0692c828b3ef085a4c4d5725bad8792
Ryan Raaum <code@raaum.org>
2009-05-23 17:44:18 -0400

    getting some simple coverage implementation going

--- oldowan/mtconvert/coverage.py
+++ oldowan/mtconvert/coverage.py
@@ -12,6 +12,29 @@ from oldowan.mtdna import rCRSplus, rCRSplus_positions
 WORD_SIZE = 15
 FORCE_SPLIT = 50
 
+class Segment(object):
+    
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+        if stop == 0:
+            # there is no zero.
+            self.stop = 16569
+
+class Coverage(object):
+    
+    def __init__(self, chunks):
+        self._chunks = chunks
+        tr = rCRSplus_positions
+        self.segments = list(Segment(tr[x[0]+1], tr[x[-1]+1]) for x in chunks)
+        self._str = self._make_str()
+
+    def __repr__(self):
+        return self._str
+
+    def _make_str(self):
+        return ','.join(list("%d:%d" % (s.start,s.stop) for s in self.segments))
+
 def calc_num_terminal_mismatches(matches):
     if matches[-1] != -1:
         return 0
@@ -25,15 +48,16 @@ def chunk(chunks, query, word_size=WORD_SIZE, force_split_at=FORCE_SPLIT):
     matches = find_match_positions(query, rCRSplus, word_size)
     num_terminal_mismatches = calc_num_terminal_mismatches(matches)
     end_of_range = len(matches) - num_terminal_mismatches
-    chunks.append(matches[:end_of_range])
+    current_chunk = matches[:end_of_range]
+    current_chunk.append(current_chunk[-(num_terminal_mismatches+1)]+word_size-1)
+    chunks.append(current_chunk)
     if num_terminal_mismatches < force_split_at:
         return chunks
     else:
         return chunk(chunks, query[-num_terminal_mismatches:], word_size)
 
 
-def find_range(query, word_size=WORD_SIZE, force_split_at=FORCE_SPLIT):
+def coverage(query, word_size=WORD_SIZE, force_split_at=FORCE_SPLIT):
     chunks = []
     chunk(chunks, query, word_size, force_split_at)
-    print chunks
-    return chunks
+    return Coverage(chunks)
--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,18 +1,29 @@
-from oldowan.mtconvert.find_range import find_range
+from oldowan.mtconvert.coverage import coverage
+from oldowan.mtconvert.sites2seq import sites2seq
 
-def test_it():
-    seq = 'TCTCATCAATACAACCCCCGCCCATCCTACCCAGCACACACACCGCTGCTAACCCCAT'
-    result = find_range(seq)
-    print result
-    #assert False
+def test_single_rcrs_segment():
+    ranges = [(16024,16365),
+              (73,340),
+              (16000,340),
+              (16000,577),
+              (577,15992),
+              (16000,16569),
+             ]
+    for item in ranges:
+        seq = sites2seq('', region=item)
+        covers = coverage(seq)
+        print covers.segments[0].start, item[0]
+        print covers.segments[0].stop, item[1]
+        assert covers.segments[0].start == item[0]
+        assert covers.segments[0].stop  == item[1]
 
 def test_gapped():
     hvr1 = 'TTCTTTCATGGGGAAGCAGATTTGGGTACCACCCAAGTATTGACTCACCCATCAACAACCGCTATGTATTTCGTACATTACTGCCAGCCACCATGAATATTGTACGGTACCATAAATACTTGACCACCTGTAGTACATAAAAACCCAATCCACATCAAAACCCCCTCCCCATGCTTACAAGCAAGTACAGCAATCAACCGTCAACTATCACACATCAACTGCAACTCCAAAGCCACCCCTCACCCACTAGGATACCAACAAACCTACCCACCCTTAACAGTACATAGTACATAAAGCCATTTACCGTACATAGCACATTACAGTCAAATCCCTTCTCGTCCC'
     hvr2 = 'ATGCACGCGATAGCATTGCGAGACGCTGGAGCCGGAGCACCCTATGTCGCAGTATCTGTCTTTGATTCCTGCCTCATCCTATTATTTATCGCACCTACGTTCAATATTACAGGCGAACATACTTACTAAAGTGTGTTTATTAATTAATGCTTGTAGGACATAATAATAACAATTGAATGTCTGCACAGCCACTTTCCACACAGACATCATAACAAAAAATTTCCACCAAACCCCCCCTCCCCCGCTTCTGGCCACAGCACTTAAACAC'
-    result = find_range(hvr1)
+    result = coverage(hvr1)
     print result
-    result = find_range(hvr2)
+    result = coverage(hvr2)
     print result
-    result = find_range(hvr1+hvr2)
+    result = coverage(hvr1+hvr2)
     print result
     #assert False
