c03693148d5c2e9aa8e4f696f7f6b8411e73d6b9
Ryan Raaum <code@raaum.org>
2009-05-27 23:25:37 -0400

    start rflp conversion process

---
+++ oldowan/mtconvert/rflp.py
@@ -0,0 +1,38 @@
+
+rflp_conversion = { '+10806_HINFI' : '10810C', # L0/L1/L5
+                    '-10806_HINFI' : '10810T', # rCRS
+                     '+4310_ALUI'  : '4312C',  # rCRS
+                     '-4310_ALUI'  : '4312T',  # L0
+                      '+9BP_DEL'   : '8281d 8282d 8283d 8284d 8285d 8286d 8287d 8288d 8289d',
+                      '-9BP_DEL'   : '',       # rCRS
+                     '+7055_ALUI'  : '7055A',  # rCRS
+                     '-7055_ALUI'  : '7055G',  # L1
+                    '+16389_HINFI' : '16390C', # L2
+                    '-16389_HINFI' : '16390G', # rCRS
+                    '+13803_HAEIII': '13803G', # L2a
+                    '-13803_HAEIII': '13803A', # rCRS
+                     '+4157_ALUI'  : '4158G',  # L2b
+                     '-4157_ALUI'  : '4158A',  # rCRS
+                     '+3592_HPAI'  : '3594T',  # L0/L1/L2/L4/L5/L6
+                     '-3592_HPAI'  : '3594C',  # rCRS
+                    '+10084_TAQI'  : '10086G', # L3b
+                    '-10084_TAQI'  : '10086A', # rCRS
+                     '+8616_MBOI'  : '8618T',  # rCRS
+                     '-8616_MBOI'  : '8618C',  # L3d
+                     '+2349_MBOI'  : '2352C',  # L3e
+                     '-2349_MBOI'  : '2352T',  # rCRS
+                     '+3693_MBOI'  : '3693G',  # rCRS
+                     '-3693_MBOI'  : '3693A',  # L2d
+                    '+10400_ALUI'  : '10398G 10400T',# M 
+                    '-10400_ALUI'  : '',       # rCRS
+                    '+10873_MNLI'  : '10873C', # rCRS
+                    '-10873_MNLI'  : '10873T', # not N
+                     '+7025_ALUI'  : '7028T',  # not H
+                     '-7025_ALUI'  : '7028C',  # rCRS
+                    '+16390_AVAII' : '16390G', # rCRS
+                    '-16390_AVAII' : '16390C', # L2
+                    '+12308_HINFI' : '12308G 12312C',# U (not finding 12312 on the tree)
+                    '-12308_HINFI' : '',       # rCRS
+                    }
+
+
